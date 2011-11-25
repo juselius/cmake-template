f0e21292a4c798bd34b5eaacfd03ff5aeddb4240
Jonas Juselius <jonas.juselius@uit.no>
2011-11-25 16:08:03 +0100

    Fixes.

--- cmake/ConfigGitRevision.cmake
+++ cmake/ConfigGitRevision.cmake
@@ -2,8 +2,7 @@ set (GIT_REVISION)
 find_package(Git)
 if (GIT_FOUND)
 	execute_process(
-		COMMAND ${GIT_EXECUTABLE} rev-list
-		--abbrev-commit --max-count=1
+		COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --max-count=1 HEAD
 		OUTPUT_VARIABLE GIT_REVISION
 		)
 	string(STRIP
