422f029ccec39da6ce7b44bb2754193a13aa3a21
Jonas Juselius <jonas.juselius@uit.no>
2011-12-08 15:49:45 +0100

    setup has better defauls.
    CommonBuildTree is "configurable" (use the source).

--- cmake/CommonBuildTree.cmake
+++ cmake/CommonBuildTree.cmake
@@ -1,18 +1,35 @@
-
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY
+if (PROJECT_RUNTIME_OUTPUT_DIR) 
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY
+        ${PROJECT_RUNTIME_OUTPUT_DIR} 
+        )
+else()
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY
         ${CMAKE_BINARY_DIR}/bin
-    )
+        )
+endif()
 
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY
+if (PROJECT_ARCHIVE_OUTPUT_DIR) 
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY
+        ${PROJECT_ARCHIVE_OUTPUT_DIR} 
+        )
+else()
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY
         ${CMAKE_BINARY_DIR}/lib
-    )
+        )
+endif()
 
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
+if (PROJECT_LIBRARY_OUTPUT_DIR) 
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
+        ${PROJECT_LIBRARY_OUTPUT_DIR} 
+        )
+else()
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
         ${CMAKE_BINARY_DIR}/lib
-    )
+        )
+endif()
 
 if (CMAKE_Fortran_COMPILER_WORKS)
-    set(CMAKE_Fortran_MODULE_DIRECTOR
+    set(CMAKE_Fortran_MODULE_DIRECTORY
         ${PROJECT_BINARY_DIR}/modules
         )
     include_directories(${PROJECT_BINARY_DIR}/modules)
