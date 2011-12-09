d4323fe5e9c3be01c01bc5c8e27951e10aaf16f4
Jonas Juselius <jonas.juselius@uit.no>
2011-12-09 18:41:25 +0100

    Bugfix.

--- cmake/ConfigureScript.cmake
+++ cmake/ConfigureScript.cmake
@@ -2,5 +2,5 @@
 function(configure_script infile outfile)
     configure_file(${infile} ${outfile} @ONLY)
     execute_process(COMMAND 
-        chmod 755 ${CMAKE_CURRENT_BINARY_DIR}/${script} OUTPUT_QUIET) 
+        chmod 755 ${outfile} OUTPUT_QUIET) 
 endfunction()
