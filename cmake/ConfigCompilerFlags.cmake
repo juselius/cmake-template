594e4049aa6cbd7d93999a514fb066a5c6667763
Jonas Juselius <jonas.juselius@uit.no>
2012-11-28 17:14:36 +0100

    Moved compiler config dir to toplevel

--- cmake/ConfigCompilerFlags.cmake
+++ cmake/ConfigCompilerFlags.cmake
@@ -1,15 +1,15 @@
 include(SaveCompilerFlags)
 
 if (CMAKE_C_COMPILER_WORKS)
-    include(${PROJECT_SOURCE_DIR}/cmake/compilers/CFlags.cmake)
+    include(${PROJECT_SOURCE_DIR}/compilers/CFlags.cmake)
 endif()
 
 if (CMAKE_CXX_COMPILER_WORKS)
-    include(${PROJECT_SOURCE_DIR}/cmake/compilers/CXXFlags.cmake)
+    include(${PROJECT_SOURCE_DIR}/compilers/CXXFlags.cmake)
 endif()
 
 if (CMAKE_Fortran_COMPILER_WORKS)
-    include(${PROJECT_SOURCE_DIR}/cmake/compilers/FortranFlags.cmake)
+    include(${PROJECT_SOURCE_DIR}/compilers/FortranFlags.cmake)
 endif()
 
 if (CMAKE_C_COMPILER_ID MATCHES Intel OR
